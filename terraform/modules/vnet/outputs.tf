a775336ebe5b287fd8f22455e01a13b9bdf33b5b
swathiraj24 <your-email@example.com>
2026-05-12 19:08:23 +0530

    Fix vnet module output for existing resource group support

--- terraform/modules/vnet/outputs.tf
+++ terraform/modules/vnet/outputs.tf
@@ -1,6 +1,6 @@
 output "resource_group_name" {
-  value       = azurerm_resource_group.this.name
-  description = "The resource group created for the hub and spoke network."
+  value = var.create_resource_group ? azurerm_resource_group.this[0].name : data.azurerm_resource_group.existing[0].name
+  description = "The resource group used for the hub and spoke network."
 }
 
 output "hub_vnet_id" {
